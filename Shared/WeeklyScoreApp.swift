bbef39f93a2cb7e2b4fa9af50f0ca97e803dc9a8
Erda Wen <wenerdaman@hotmail.com>
2021-07-03 22:36:59 -0700

    Initial Commit

---
+++ Shared/WeeklyScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyScoreApp.swift
+//  Shared
+//
+//  Created by Xiaozhen Yang on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
