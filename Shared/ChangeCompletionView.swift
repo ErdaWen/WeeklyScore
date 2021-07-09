1af7edf29f690f28a4aee8c0ec33b6e72c9a4878
Erda Wen <wenerdaman@hotmail.com>
2021-07-09 00:01:04 -0700

    - Finish interaction between status / score/ hours in ChangeCompletion View

--- Shared/ChangeCompletionView.swift
+++ Shared/ChangeCompletionView.swift
@@ -27,6 +27,8 @@ struct ChangeCompletionView: View {
     // This var should be adjusted with the inputchecked at the same time
     // 0: messed up, 1: partial credit, 2: complete, 3: extra
     @State var completeState = 0
+    // passivestateChange is false if the picker is jumped because of input hour is not as required
+    @State var activeStateChange = true
     
     
     var body: some View {
@@ -36,22 +38,46 @@ struct ChangeCompletionView: View {
                     // MARK:Duration-based case
                     Picker("", selection:$completeState){
                         Text("Messed up").tag(0)
-                        Text("Manual").tag(1)
+                        Text("Partial").tag(1)
                         Text("Compelete").tag(2)
                         Text("Plus").tag(3)
                     }
                     .pickerStyle(SegmentedPickerStyle())
+                    //update check status with given picker
                     .onChange(of: completeState, perform: { value in
                         if completeState == 0 {
+                            // No score, 0 score, 0 hour
                             inputChecked = false
                             inputScoreGained = 0
                             inputHoursGained = 0.0
+                            inputHoursGainedString = String(inputHoursGained)
                         } else if (completeState == 1){
+                            // Partial score, leave the values to be entered in the textfields.
                             inputChecked = true
+                            if activeStateChange{
+                                inputScoreGained = entryModel.entries[entryIndex].score
+                                inputHoursGained = habitHours
+                                inputHoursGainedString = String(inputHoursGained)
+                            } else {
+                                // Passive change to partial state, entered hours is more than full hours
+                                inputScoreGained = entryModel.entries[entryIndex].score
+                                // Reset activeStateChange
+                                activeStateChange = true
+                            }
+                            
                         } else if (completeState == 3){
+                            // Extra, full socre, hours to be entered, default at full hours
                             inputChecked = true
-                            inputScoreGained = entryModel.entries[entryIndex].score
-                            inputHoursGained = habitHours
+                            if activeStateChange{
+                                inputScoreGained = entryModel.entries[entryIndex].score
+                                inputHoursGained = habitHours
+                                inputHoursGainedString = String(inputHoursGained)
+                            } else {
+                                // Passive change to partial state, entered hours is less than full hours
+                                inputScoreGained = entryModel.entries[entryIndex].score
+                                // Reset activeStateChange
+                                activeStateChange = true
+                            }
                         } else {
                             inputChecked = true
                             inputScoreGained = entryModel.entries[entryIndex].score
@@ -59,20 +85,73 @@ struct ChangeCompletionView: View {
                             inputHoursGainedString = String(inputHoursGained)
                         }
                     })
-                    if completeState == 2 {
-                        Text ("Full credit, score = \(inputScoreGained), hoursgained = \(inputHoursGained)")
-                    } else if (completeState == 3){
-                        HStack{
-                            Text ("Full credit, score = \(inputScoreGained)")
-                            TextField("Hours", text: $inputHoursGainedString)
-                                .onChange(of: inputHoursGainedString, perform: { value in
-                                    if let inputnumber = Double(inputHoursGainedString) {
-                                        inputHoursGained = inputnumber
-                                    }
-                                })
-                        }
+                    
+                    HStack{
+                        Stepper("Score: \(inputScoreGained)", value: $inputScoreGained, in: 0...entryModel.entries[entryIndex].score)
+                            .disabled(completeState != 1)
+                        TextField("Hours", text: $inputHoursGainedString)
+                            .keyboardType(.numberPad)
+                            .disabled((completeState == 0)||(completeState == 2))
+                            .onChange(of: inputHoursGainedString, perform: { value in
+                                if let inputnumber = Double(inputHoursGainedString) {
+                                    inputHoursGained = inputnumber
+                                }
+                                if (inputHoursGained > habitHours)&&(completeState == 1) {
+                                    // If input hours is large than full hours in partial state, trigger passive state chagne
+                                    activeStateChange = false
+                                    //
+                                    completeState = 3
+                                } else if (inputHoursGained < habitHours)&&(completeState == 3){
+                                    // If input hours is less than full hours in partial state, trigger passive state chagne
+                                    activeStateChange = false
+                                    //
+                                    completeState = 1
+                                }
+                            })
                     }
                     
+                    // Texts or Fields depending on the status
+                    //                    if completeState == 2 {
+                    //                        Text ("Full credit, score = \(inputScoreGained), hoursgained = \(inputHoursGained)")
+                    //                    } else if (completeState == 3){
+                    //                        HStack{
+                    //                            Text ("Full credit, score = \(inputScoreGained)")
+                    //                            TextField("Hours", text: $inputHoursGainedString)
+                    //                                .keyboardType(.numberPad)
+                    //                                .onChange(of: inputHoursGainedString, perform: { value in
+                    //                                    if let inputnumber = Double(inputHoursGainedString) {
+                    //                                        inputHoursGained = inputnumber
+                    //                                    }
+                    //                                    if inputHoursGained < habitHours {
+                    //                                        // If input hours is less than full hours, trigger passive state chagne
+                    //                                        activeStateChange = false
+                    //                                        //
+                    //                                        completeState = 1
+                    //                                    }
+                    //                                })
+                    //                        }
+                    //                    } else if (completeState == 1){
+                    //                        HStack{
+                    //                            Stepper("Score: \(inputScoreGained)", value: $inputScoreGained, in: 0...entryModel.entries[entryIndex].score)
+                    //                            TextField("Hours", text: $inputHoursGainedString)
+                    //                                .keyboardType(.numberPad)
+                    //                                .onChange(of: inputHoursGainedString, perform: { value in
+                    //                                    if let inputnumber = Double(inputHoursGainedString) {
+                    //                                        inputHoursGained = inputnumber
+                    //                                    }
+                    //                                    if inputHoursGained > habitHours {
+                    //                                        // If input hours is less than full hours, trigger passive state chagne
+                    //                                        activeStateChange = false
+                    //                                        //
+                    //                                        completeState = 3
+                    //                                    }
+                    //                                })
+                    //
+                    //                        }
+                    //                    } else {
+                    //                        // completeState == 0, nothing shown
+                    //                    }
+                    //
                 } else {
                     // MARK: Hit-based case
                 }
@@ -85,6 +164,7 @@ struct ChangeCompletionView: View {
             }
             inputScoreGained = entryModel.entries[entryIndex].scoreGained
             inputHoursGained = entryModel.entries[entryIndex].hoursGained
+            inputHoursGainedString = String(inputHoursGained)
             inputChecked = entryModel.entries[entryIndex].checked
             orgScoreGained = inputScoreGained
             orgHoursGained = inputHoursGained
