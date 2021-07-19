3e6dec61d9e12b13bcc93858429302778f767013
ErdaWen <ewen@eng.ucsd.edu>
2021-07-18 22:49:37 -0700

    - Add core data

---
+++ Data/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  WeeklyScore
+//
+//  Created by Erda Wen on 7/18/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ Data/Schedule+CoreDataProperties.swift
@@ -0,0 +1,53 @@
+//
+//  Schedule+CoreDataProperties.swift
+//  WeeklyScore
+//
+//  Created by Erda Wen on 7/18/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Schedule {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Schedule> {
+        return NSFetchRequest<Schedule>(entityName: "Schedule")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var score: Int64
+    @NSManaged public var hidden: Bool
+    @NSManaged public var beginTime: Date
+    @NSManaged public var location: String?
+    @NSManaged public var notes: String?
+    @NSManaged public var journal: String?
+    @NSManaged public var scoreGained: Int64
+    @NSManaged public var checked: Bool
+    @NSManaged public var endTime: Date
+    @NSManaged public var minutesGained: Int64
+    @NSManaged public var items: NSSet
+
+}
+
+// MARK: Generated accessors for items
+extension Schedule {
+
+    @objc(addItemsObject:)
+    @NSManaged public func addToItems(_ value: Item)
+
+    @objc(removeItemsObject:)
+    @NSManaged public func removeFromItems(_ value: Item)
+
+    @objc(addItems:)
+    @NSManaged public func addToItems(_ values: NSSet)
+
+    @objc(removeItems:)
+    @NSManaged public func removeFromItems(_ values: NSSet)
+
+}
+
+extension Schedule : Identifiable {
+
+}
