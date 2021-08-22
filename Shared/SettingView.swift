9ef4e7981c5acab058adb0afa0acf7eb0287113a
ErdaWen <ewen@eng.ucsd.edu>
2021-08-21 23:22:41 -0700

    - Same as last version

--- Shared/SettingView.swift
+++ Shared/SettingView.swift
@@ -8,40 +8,18 @@
 import SwiftUI
 
 struct SettingView: View {
-    @AppStorage("nightMode") private var nightMode = true
-    @AppStorage("weekStartDay") private var weekStartDay = 0
-    @AppStorage("autoCompleteMode") private var autoCompleteMode = 1
+
     
-    
+    init(){
+        Theme.navigationBarColors(background: UIColor(Color("tag_color_green").opacity(0.3)), titleColor: UIColor(Color("text_black")))
+        }
     
     var body: some View {
-        VStack{
-            ZStack(alignment:.top){
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color("tag_color_red").opacity(0.1))
-                VStack(alignment:. leading){
-                    Picker("Period starts from", selection: $weekStartDay) {
-                        Text("Sunday").tag(0)
-                        Text("Monday").tag(1)
-                        Text("Tuesday").tag(2)
-                        Text("Wednesday").tag(3)
-                        Text("Thursday").tag(4)
-                        Text("Friday").tag(5)
-                        Text("Saturday").tag(6)
-                    }.pickerStyle(MenuPickerStyle())
-                    .foregroundColor(Color("text_black"))
-                    
-                }
-            }.frame(height:400)
-            
-            
-            
-        }.padding(20)
-        
-        
-        
-        
-        
+        NavigationView{
+            SettingViewContent()
+                .navigationTitle("⚙️ Settings")
+        }//End navigation View
+    
         
     }
 }
