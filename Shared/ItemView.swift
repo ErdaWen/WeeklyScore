e8ab6411771d65e3d1ff5014e95e288e52aed273
ErdaWen <ewen@eng.ucsd.edu>
2022-10-02 15:17:12 -0700

    - Reposition button positions  in habit view

--- Shared/ItemView.swift
+++ Shared/ItemView.swift
@@ -21,14 +21,15 @@ struct ItemView: View {
     
     @State var changeId = 0
     @State var showArchive = UserDefaults.standard.bool(forKey: "showArchivedItem")
-    @State var catagorized = UserDefaults.standard.bool(forKey: "itemCatagorized")
+    // @State var catagorized = UserDefaults.standard.bool(forKey: "itemCatagorized")
+    let catagorized = true
     
     let pButton:CGFloat = 20
     let sButton:CGFloat = 22
     let pButton2:CGFloat = 10
     let mButtons:CGFloat = 35
     let mButtonText:CGFloat = -3
-    let fsSub:CGFloat = 12
+    let fsSub:CGFloat = 10
     
     var body: some View {
         ZStack(alignment: .top){
@@ -48,85 +49,55 @@ struct ItemView: View {
             }
             //MARK: Add habit button
                 //MARK:Buttons
-            HStack(spacing: 20){
+            HStack(alignment:.center,spacing: 20){
                 FloatButton(systemName: "plus.square", sButton: sButton) {
                     addViewPresented = true
                 }
-                .padding(.top,pButton)
+                .padding(.leading,20)
                 .sheet(isPresented: $addViewPresented, content: {
                     AddItemView(addItemViewPresented: $addViewPresented)
                         .environment(\.managedObjectContext,self.viewContext)
                 })
                 
                 if catagorized{
-                    FloatButton(systemName: "folder", sButton: sButton) {
+                    FloatButton(systemName: "plus.rectangle.on.folder", sButton: sButton+5) {
                         tagViewPresented = true
                     }
-                    .padding(.top,pButton)
                     .sheet(isPresented: $tagViewPresented) {
                         EditTagView(editTagViewPresented: $tagViewPresented)
                             .environment(\.managedObjectContext,self.viewContext)
                     }
                 }
-            }.frame(height:80)
+                
+                HStack(alignment: .center, spacing: mButtonText){
+                    FloatButton(systemName: showArchive ? "archivebox.fill" : "archivebox", sButton: sButton) {
+                        withAnimation(.default){
+                        showArchive.toggle()
+                        }
+                        UserDefaults.standard.set(showArchive, forKey: "showArchivedItem")
+                    }
+                    Text( showArchive ? "Hide\nArchived" : "Show\nArchived")
+                        .font(.system(size: fsSub))
+                        .fontWeight(.light)
+                        .foregroundColor(Color("text_black"))
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            .frame(height:80)
+            .padding(.top,pButton)
             
             //MARK: Archive and tag button
             VStack{
                 Spacer()
-                ZStack(){
-                    //MARK: Background
-                        VStack{
-                            Spacer()
-                            Color.clear
-                                .background(.ultraThinMaterial)
-                                .blur(radius: 10)
-                        }
-
-                    //MARK: Buttons on top
-                    HStack(spacing:0){
-                        Spacer()
-                        //MARK: Archive button
-                        HStack(alignment: .center, spacing: mButtonText){
-                            FloatButton(systemName: showArchive ? "archivebox.fill" : "archivebox", sButton: sButton) {
-                                withAnimation(.default){
-                                showArchive.toggle()
-                                }
-                                UserDefaults.standard.set(showArchive, forKey: "showArchivedItem")
-                            }
-                            Text( showArchive ? "Hide\nArchived" : "Show\nArchived")
-                                .font(.system(size: fsSub))
-                                .fontWeight(.light)
-                                .foregroundColor(Color("text_black"))
-                                .multilineTextAlignment(.leading)
-
-                        }
-                        Spacer()
-                        //MARK: Tag Button
-                        HStack(alignment: .center, spacing: mButtonText){
-                            FloatButton(systemName: catagorized ? "folder.fill" : "folder", sButton: sButton) {
-                                withAnimation(.default){
-                                    catagorized.toggle()
-                                }
-                                    UserDefaults.standard.set(catagorized, forKey: "itemCatagorized")
-                                }
-                            
-
-                            Text( catagorized ? "Hide\nCatagory" : "Show\nCatagory")
-                                .font(.system(size: fsSub))
-                                .fontWeight(.light)
-                                .foregroundColor(Color("text_black"))
-                                .multilineTextAlignment(.leading)
-                                .frame(width:70)
-
-                        }
-                        Spacer()
-                    }
-                    .padding(.bottom,45)
-                }.frame(height:45+45)//end bottom button Zstack
-        
-            } //end bottom button Vstack
+                Color.clear
+                    .ignoresSafeArea()
+                    .background(.ultraThinMaterial)
+                    .blur(radius: 10)
+                    .frame(height:60)//end bottom button Zstack
+            }
             
-            
+
+   
         }
         // end whole view ZStack
         
